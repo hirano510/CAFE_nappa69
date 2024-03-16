915369b788d560b7b00a658c3f4fef930be3db2e
takamasa hirano <pingyexiaoshang@gmail.com>
2024-03-16 14:37:46 +0900

    cafeサイト　コミット

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.mv').slick({
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 1200,
+    arrows: false,
+    swipe: false,
+})
+
+Fancybox.bind("[data-fancybox]", {
+    // Your custom options
+});
